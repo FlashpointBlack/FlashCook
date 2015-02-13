a45ec57248bb780a3734ad4fb763a8043f9ea812
FlashpointBlack <d.hollings.83@gmail.com>
2015-02-13 00:14:46 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/FlashpointBlack/FlashCook/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.FlashpointBlack.FlashCook;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
